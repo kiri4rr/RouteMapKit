6eb26b2e82354022b4f039cec0308f156d5cd89b
kiri4rr <kirill.romanenko2000@gmail.com>
2023-03-09 13:28:47 +0200

    Initial Commit

---
+++ RouteAtMapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouteAtMapKit
+//
+//  Created by Kirill Romanenko on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
